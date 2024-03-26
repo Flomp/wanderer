aca94fd803f81292ee5bd70bc1f005a5b43129fc
Christian Beutel <>
2024-03-26 15:24:43 +0100

    fixes summit log date formatting

--- web/src/lib/components/summit_log/summit_log_card.svelte
+++ web/src/lib/components/summit_log/summit_log_card.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     import type { SummitLog } from "$lib/models/summit_log";
     import { format, parse } from "date-fns";
-    import Dropdown, { type DropdownItem } from "../base/dropdown.svelte";
     import { _ } from "svelte-i18n";
+    import Dropdown, { type DropdownItem } from "../base/dropdown.svelte";
 
     export let log: SummitLog;
     export let mode: "show" | "edit" = "show";
@@ -15,7 +15,12 @@
 
 <div class="p-4 my-2 border border-input-border rounded-xl">
     <div class="flex justify-between items-center mb-2">
-        <h5 class="font-medium mr-2">{format(log.date, "dd.MM.yyyy")}</h5>
+        <h5 class="font-medium mr-2">
+            {parse(log.date, "yyyy-MM-dd", new Date()).toLocaleDateString(
+                undefined,
+                { month: "2-digit", day: "2-digit", year: "numeric" },
+            )}
+        </h5>
 
         {#if mode == "edit"}
             <Dropdown items={dropdownItems} on:change></Dropdown>
