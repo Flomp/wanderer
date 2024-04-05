b2d4ad05d8637171fb606ed59fbf9f54415d3c45
Christian Beutel <>
2024-04-05 16:59:26 +0200

    fixes public trail filter values bug

--- web/src/routes/api/v1/trail/filter/+server.ts
+++ web/src/routes/api/v1/trail/filter/+server.ts
@@ -3,10 +3,18 @@ import { pb } from '$lib/pocketbase';
 import { error, json, type RequestEvent } from '@sveltejs/kit';
 
 export async function GET(event: RequestEvent) {
+    if (!pb.authStore.model) {
+        return json({
+            min_distance: 0,
+            max_distance: 20000,
+            min_elevation_gain: 0,
+            max_elevation_gain: 4000
+        });
+    }
     try {
         const r = await pb.collection('trails_filter').getOne<TrailFilterValues>(pb.authStore.model!.id)
         return json(r)
     } catch (e: any) {
-        throw error(e.status, e);
+        throw error(e.status || 500, e);
     }
 }
