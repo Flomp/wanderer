1844edb5376f2b950847d745811c7f880b957367
tofublock <tobias.ferner@gmail.com>
2024-12-06 20:49:53 +0100

    trail sorting by duration and author

---
+++ db/migrations/1733513436_update_sortable_attributes_duration.go
@@ -0,0 +1,36 @@
+package migrations
+
+import (
+	"os"
+
+	"github.com/meilisearch/meilisearch-go"
+	"github.com/pocketbase/dbx"
+
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	client := meilisearch.New(os.Getenv("MEILI_URL"), meilisearch.WithAPIKey(os.Getenv("MEILI_MASTER_KEY")))
+
+	m.Register(func(db dbx.Builder) error {
+		_, err := client.Index("trails").UpdateSortableAttributes(&[]string{
+			"created", "date", "difficulty", "distance", "elevation_gain", "name", "duration", "author",
+		})
+
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}, func(db dbx.Builder) error {
+		_, err := client.Index("trails").UpdateSortableAttributes(&[]string{
+			"created", "date", "difficulty", "distance", "elevation_gain", "name",
+		})
+
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
