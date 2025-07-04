819aea801e4bd3ffe566b6b7ec4539e05c4a4b9d
Christian Beutel <>
2025-07-04 10:16:11 +0200

    return json errors from valhalla api route

--- web/src/routes/api/v1/valhalla/route/+server.ts
+++ web/src/routes/api/v1/valhalla/route/+server.ts
@@ -5,16 +5,17 @@ import { error, json, type NumericRange, type RequestEvent } from "@sveltejs/kit
 export async function POST(event: RequestEvent) {
     const data = await event.request.json()
     if (!env.PUBLIC_VALHALLA_URL) {
-        return error(400, "PUBLIC_VALHALLA_URL not set")
+        return json({ message: "PUBLIC_VALHALLA_URL not set" }, { status: 400 })
     }
     try {
-        const r = await event.fetch(env.PUBLIC_VALHALLA_URL + '/route', { method: "POST", body: JSON.stringify(data) });        
+        const r = await event.fetch(env.PUBLIC_VALHALLA_URL + '/route', { method: "POST", body: JSON.stringify(data) });
         const response = await r.json();
         if (!r.ok) {
-            throw error(r.status as NumericRange<400,500>, response);
+            return json({ message: response }, { status: r.status })
+
         }
         return json(response);
     } catch (e: any) {
-        throw error(e.status || 500, e)
+        return json({ message: e }, { status: 500 })
     }
 }
